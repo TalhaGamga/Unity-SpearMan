f26965cb8f914e19304c6fd70b8fa76ab8808620
Talha Gamga <89526484+TalhaGamga@users.noreply.github.com>
2026-07-23 01:51:29 +0300

    feat: apply physics impact to test box

--- Assets/GAME/Scripts/TestBox.cs
+++ Assets/GAME/Scripts/TestBox.cs
@@ -4,8 +4,8 @@ using DinoFracture;
 public class TestBox : MonoBehaviour, IDamageable, IImpactable, IDestructible
 {
     private Rigidbody _rb;
-    [SerializeField] private PreFracturedGeometry _preFractured;
-    [SerializeField] private RuntimeFracturedGeometry _runtimeFractured;
+    //[SerializeField] private PreFracturedGeometry _preFractured;
+    //[SerializeField] private RuntimeFracturedGeometry _runtimeFractured;
 
     private void Awake()
     {
@@ -18,23 +18,32 @@ public class TestBox : MonoBehaviour, IDamageable, IImpactable, IDestructible
 
     public void Destruct(DestructData data)
     {
-        if (_preFractured != null)
-        {
-            _preFractured.Fracture();
-        }
+        //if (_preFractured != null)
+        //{
+        //    _preFractured.Fracture();
+        //}
 
-        else if (_runtimeFractured != null)
-        {
-            _runtimeFractured.Fracture();
-        }
+        //else if (_runtimeFractured != null)
+        //{
+        //    _runtimeFractured.Fracture();
+        //}
 
-        else
-        {
-            Debug.Log($"DinoDestructible on {gameObject.name} has no fracture component attached.");
-        }
+        //else
+        //{
+        //    Debug.Log($"DinoDestructible on {gameObject.name} has no fracture component attached.");
+        //}
     }
 
     public void ApplyImpact(ImpactData data)
     {
+        if (_rb == null)
+            return;
+
+        Vector3 force = data.Direction.normalized * data.Force;
+
+        if (data.Point != Vector3.zero)
+            _rb.AddForceAtPosition(force, data.Point, ForceMode.Impulse);
+        else
+            _rb.AddForce(force, ForceMode.Impulse);
     }
 }
